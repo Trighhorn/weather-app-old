df93b302159dbb16c44636eb5021abe3b79b5ff9
Abrahan Gonzalez <trighhorn@gmail.com>
2020-06-02 16:35:20 -0600

    init commit

---
+++ src/compnents/card/card-item.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardItem = props => {
+    return (
+        <div className="card-item">
+            <p>Hello from the card component</p>
+        </div>
+    )
+};
+
+export default CardItem;
