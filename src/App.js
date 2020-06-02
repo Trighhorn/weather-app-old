64679ce49c38f5839268e9ea2143b5e9fb2d911b
Abrahan Gonzalez <trighhorn@gmail.com>
2020-06-02 17:45:06 -0600

    added env file for api keys

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CardItem from './compnents/card/card-item';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
--- src/compnents/card/card-item.js
+++ src/compnents/card/card-item.js
@@ -1,9 +1,28 @@
 import React from "react";
+import axios from 'axios';
+
 
 const CardItem = props => {
+    // state = {
+
+    // }
     return (
-        <div className="card-item">
-            <p>Hello from the card component</p>
+        <div className="card-container">
+            <div className="card-item">
+                <p>1</p>
+            </div>
+            <div className="card-item">
+                <p>2</p>
+            </div>
+            <div className="card-item">
+                <p>3</p>
+            </div>
+            <div className="card-item">
+                <p>4</p>
+            </div>
+            <div className="card-item">
+                <p>5</p>
+            </div>
         </div>
     )
 };
